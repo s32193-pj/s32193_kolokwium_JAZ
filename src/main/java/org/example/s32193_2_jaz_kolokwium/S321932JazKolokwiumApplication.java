94be74bda055e3beb42e3f1a795deeb287a042d2
s32193-pj <146555805+s32193-pj@users.noreply.github.com>
2025-06-21 11:31:11 +0200

    update 2

--- src/main/java/org/example/s32193_2_jaz_kolokwium/S321932JazKolokwiumApplication.java
+++ src/main/java/org/example/s32193_2_jaz_kolokwium/S321932JazKolokwiumApplication.java
@@ -2,9 +2,8 @@ package org.example.s32193_2_jaz_kolokwium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class S321932JazKolokwiumApplication {
 
     public static void main(String[] args) {
