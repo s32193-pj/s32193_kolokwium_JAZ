f8378f8acd76000d8952e1e7a786d9d373ed67e1
s32193-pj <146555805+s32193-pj@users.noreply.github.com>
2025-06-21 10:11:20 +0200

    update 0

---
+++ src/main/java/org/example/s32193_2_jaz_kolokwium/S321932JazKolokwiumApplication.java
@@ -0,0 +1,13 @@
+package org.example.s32193_2_jaz_kolokwium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S321932JazKolokwiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S321932JazKolokwiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/s32193_2_jaz_kolokwium/S321932JazKolokwiumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.s32193_2_jaz_kolokwium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S321932JazKolokwiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
